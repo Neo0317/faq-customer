f9b15f73ee6d2d51cf357c12b1346ba6f10f6942
neovyij@gmail.com <neovyij@gmail.com>
2025-05-01 13:28:10 -0500

    1

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def load_faq():
     db = FAISS.from_documents(documents, embeddings)
     return db
 
-retriever = load_faq().as_retriever(search_kwargs={"k": 10})
+retriever = load_faq().as_retriever(search_kwargs={"k": 5})
 
 query = st.text_input(text["input_label"], placeholder=text["input_placeholder"])
 
